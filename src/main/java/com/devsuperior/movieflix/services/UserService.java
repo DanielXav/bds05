088bcc70be8a89a3bdb3de2f5a7828e1fbc6873c
Daniel Xavier <dxavier194@gmail.com>
2022-04-13 16:45:48 -0300

    Finished project

--- src/main/java/com/devsuperior/movieflix/services/UserService.java
+++ src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -5,15 +5,30 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.movieflix.dto.UserDTO;
 import com.devsuperior.movieflix.entities.User;
 import com.devsuperior.movieflix.repositories.UserRepository;
+import com.devsuperior.movieflix.services.exceptions.UnauthorizedException;
 
 @Service
 public class UserService implements UserDetailsService {
 	
 	@Autowired
 	private UserRepository repository;
+	
+	@Autowired 
+	private AuthService authService;
+	
+	@Transactional(readOnly = true)
+	public UserDTO profile() {
+		User user = authService.authenticated();
+		if (user == null) {
+			throw new UnauthorizedException("User not found");
+		}
+		return new UserDTO(user); 
+	}
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
